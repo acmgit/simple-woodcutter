08bf646035e3193b557d0cedad7cb7e0c8531324
A.C.M <undertakers_help@yahoo.com>
2023-05-12 05:40:53 +0200

    Dig leaves and sneak key.

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ local max_radius = tonumber(ms:get "simple_woodcutter.max_radius") or 1
 local delay = tonumber(ms:get "simple_woodcutter.delay") or 0.01
 
 local function chop_around(pos, oldnode, digger)
-  local next_pos = minetest.find_node_near(pos, max_radius, oldnode.name)
+  local next_pos = minetest.find_node_near(pos, max_radius, {oldnode.name, "group:leaves"})
   if not next_pos then return end
   local digger_pos = digger:get_pos()
   if not digger_pos then return end
@@ -17,5 +17,6 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
   if not minetest.registered_nodes[oldnode.name].groups.tree then return end
   if not digger:get_wielded_item():get_definition().groups.axe then return end
   if digger:get_hp() == 0 then return end
+  if not digger:get_player_control().sneak then return end
   minetest.after(delay, chop_around, pos, oldnode, digger)
 end)
