fc3dca78062498c90a9af76cb20cb556d119f296
A.C.M <undertakers_help@yahoo.com>
2023-05-12 21:48:32 +0200

    Recursion changed to loop.

--- init.lua
+++ init.lua
@@ -1,22 +1,24 @@
-local ms = minetest.settings
+local ms, mt = minetest.settings, minetest
 local max_distance = tonumber(ms:get "simple_woodcutter.max_distance") or 40
 local max_radius = tonumber(ms:get "simple_woodcutter.max_radius") or 1
-local delay = tonumber(ms:get "simple_woodcutter.delay") or 0.01
 
 local function chop_around(pos, oldnode, digger)
-  local next_pos = minetest.find_node_near(pos, max_radius, {oldnode.name, "group:leaves"})
-  if not next_pos then return end
-  local digger_pos = digger:get_pos()
-  if not digger_pos then return end
-  if pos:distance(digger_pos) > max_distance then return end
-  minetest.node_dig(next_pos, minetest.get_node(next_pos), digger)
-  minetest.after(delay, chop_around, pos, oldnode, digger)
+  local next_pos, digger_pos
+  for distance = 1, max_distance do
+      next_pos = mt.find_node_near(pos, max_radius, {oldnode.name, "group:leaves"})
+      if not next_pos then break end
+      digger_pos = digger:get_pos()
+      if not digger_pos then break end
+      if pos:distance(digger_pos) > max_distance then break end
+      mt.node_dig(next_pos, mt.get_node(next_pos), digger)
+      pos = next_pos
+  end
 end
 
-minetest.register_on_dignode(function(pos, oldnode, digger)
-  if not minetest.registered_nodes[oldnode.name].groups.tree then return end
+mt.register_on_dignode(function(pos, oldnode, digger)
+  if not mt.registered_nodes[oldnode.name].groups.tree then return end
   if not digger:get_wielded_item():get_definition().groups.axe then return end
   if digger:get_hp() == 0 then return end
-  if digger:get_player_control().sneak then return end
-  minetest.after(delay, chop_around, pos, oldnode, digger)
+  if not digger:get_player_control().sneak then return end
+  chop_around(pos, oldnode, digger)
 end)
