e5e652ee90c208c870f26ffdcbff7266aa9c9139
A.C.M <undertakers_help@yahoo.com>
2023-05-18 15:08:00 +0200

    Sneak chancel.

--- init.lua
+++ init.lua
@@ -28,7 +28,7 @@ mt.register_on_dignode(function(pos, oldnode, digger)
   if not mt.check_player_privs(digger:get_player_name(), {lumberjack=true}) then return end
   if not digger:get_wielded_item():get_definition().groups.axe then return end
   if digger:get_hp() == 0 then return end
-  if not mt.registered_node[oldnode.name] then return end
+  if not mt.registered_nodes[oldnode.name] then return end
   if not mt.registered_nodes[oldnode.name].groups.tree then return end
 
 
